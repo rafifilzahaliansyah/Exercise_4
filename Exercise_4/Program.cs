ac7c2471d2bdf98717c280cfd53dce7be383a48f
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-12-05 15:37:56 +0700

    Add project files.

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class stack
+    {
+        public int[] a = new int[52];
+        public int i = 0;
+
+        public int count()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
