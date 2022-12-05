78e149798963aff276b23702913ffa26b2f02079
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2022-12-05 15:43:51 +0700

    membuat push

--- Exercise_4/Program.cs
+++ Exercise_4/Program.cs
@@ -11,6 +11,19 @@ namespace Exercise_4
         public int[] a = new int[52];
         public int i = 0;
 
+        public void push(int e)
+        {
+            int f = count();
+            if(f==a.Length)
+            {
+                Console.WriteLine("stack is full");
+            }
+            else
+            {
+                a[i] = e;
+                i = i + 1;
+            }
+        }
         public int count()
         {
 
